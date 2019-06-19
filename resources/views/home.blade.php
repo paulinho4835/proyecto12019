4b056b3efaa21c611ba8a8855aba333714924373
Paulinho4835 <paulinho4835@gmail.com>
2019-06-19 00:15:15 -0400

    boton

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,7 @@
         <div>
 
             <button onclick="window.location='{{ url('/lista3') }}'" class="btn btn-default">Invertir</button>
+            <button href="" class="btn btn-default">Mis inversiones</button>
             <button onclick="window.location='{{ url('/lista') }}'" type="button" id="crearproyecto" class="btn btn-success">Crear Proyecto</button>
             <button onclick="window.location='{{ url('/lista2') }}'" type="button" class="btn btn-success">Calcular Presupuesto</button>
             <button onclick="window.location='{{ url('/comprados') }}'" type="button" class="btn btn-success">Proyectos Comprados</button>
