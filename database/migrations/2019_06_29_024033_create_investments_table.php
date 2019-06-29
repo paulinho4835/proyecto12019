16e4f9f677e36f0e31413ce6b4311ed162828ec9
Paulinho4835 <paulinho4835@gmail.com>
2019-06-28 22:43:45 -0400

    nueva tabla

---
+++ database/migrations/2019_06_29_024033_create_investments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvestmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('investments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->Integer('usuario');
+            $table->Integer('proyecto');
+            $table->float('monto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('investments');
+    }
+}
