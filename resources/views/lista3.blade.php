6cb6306b74c48fe841722c8a10bd52a025f80b40
Paulinho4835 <paulinho4835@gmail.com>
2019-07-02 00:19:36 -0400

    lista tabla

--- resources/views/lista3.blade.php
+++ resources/views/lista3.blade.php
@@ -2,30 +2,44 @@
 
 @section('content')
 
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+
 
     <div class="section-top-border">
-        <h3 class="mb-30">Lista De Proyectos</h3>
+        <center><h3 class="mb-30">Lista De Proyectos</h3></center>
+        <br><br>
         <div class="progress-table-wrap">
             <div class="progress-table">
                 <div class="table-head">
-                    <div class="visit">Nombre</div>
                 </div>
 
 
+                <table align="center" style="width:70%">
+                <tr><th>Nombre del proyecto</th><th></th></tr>
                 @foreach ($tipos as $t)
                         <div class="table-row">
-                            <div class="visit">{{ $t->titulo }}</div>
+
+                                <tr>
+                                    <td><div class="visit">{{ $t->titulo }}</div></td>
 
                             <div class="serial">
 
                                     <form action="{{ route('invertir') }}" method="POST">
                                         @csrf
                                         <input type="hidden" name="id_tipo" value="{{ $t->id }}">
-                                        <input type="submit" value="Invertir" class="genric-btn danger">
+                                        <td><input type="submit" value="Invertir" class="genric-btn danger"></td>
                                     </form>
                             </div>
+                                </tr>
+
                         </div>
                 @endforeach
+                </table>
             </div>
 
             <center><a href="{{ url('/home') }}" class="btn btn-info" role="button">Regresar</a></center>
