5b8204472a4149327b4b2dc47f4dcc6b43bef15a
Paulinho4835 <paulinho4835@gmail.com>
2019-07-02 19:42:22 -0400

    mis inversiones

--- resources/views/misinv.blade.php
+++ resources/views/misinv.blade.php
@@ -2,35 +2,49 @@
 
 @section('content')
 
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+
 
     <div class="section-top-border">
-        <h3 class="mb-30">Lista De Proyectos</h3>
+        <center><h3 class="mb-30">Mis Inversiones</h3></center>
+        <br><br>
         <div class="progress-table-wrap">
             <div class="progress-table">
                 <div class="table-head">
-                    <div class="visit">Nombre</div>
+
                 </div>
 
 
+                <table align="center" style="width:70%">
+                    <tr><th>Nombre del proyecto</th><th>Monto</th></tr>
                 @foreach ($proy as $t)
                 @foreach ($tipos as $ti)
                         @if ($ti->proyecto==$t->id)
                             @if ($ti->usuario==$id)
                     <div class="table-row">
 
-                        <div class="visit">{{ $t->titulo }}</div>
+                        <tr>
+                            <td><div class="visit">{{ $t->titulo }}</div></td>
 
                         <div class="serial">
 
-                            <div class="visit">{{ $ti->monto }}</div>
+                            <td><div class="visit">{{ $ti->monto }}</div></td>
 
                         </div>
+                        </tr>
                     </div>
                         @endif
                         @endif
                 @endforeach
                     @endforeach
+                </table>
             </div>
+            <br>
 
             <center><a href="{{ url('/home') }}" class="btn btn-info" role="button">Regresar</a></center>
         </div>
