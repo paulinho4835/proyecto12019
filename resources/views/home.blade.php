0e90cd1f0f6c338f01041548ccee039271d98b58
Paulinho4835 <paulinho4835@gmail.com>
2019-07-02 19:52:10 -0400

    proyectos creados

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
             <button onclick="window.location='{{ url('/lista3') }}'" class="btn btn-success">Invertir</button>
             <button onclick="window.location='{{ url('/misinv') }}'" class="btn btn-success">Mis inversiones</button>
             <button onclick="window.location='{{ url('/lista') }}'" type="button" id="crearproyecto" class="btn btn-success">Crear Proyecto</button>
-            <button onclick="window.location='{{ url('/lista2') }}'" type="button" class="btn btn-success">Calcular Presupuesto</button>
+            <button onclick="window.location='{{ url('/lista2') }}'" type="button" class="btn btn-success">Proyectos Creados</button>
             <button onclick="window.location='{{ url('/comprados') }}'" type="button" class="btn btn-success">Proyectos Comprados</button>
             <button onclick="window.location='{{ url('/venta') }}'" type="button" class="btn btn-success">Proyectos en venta</button>
             <button onclick="window.location='{{ url('/venta2') }}'" type="button" class="btn btn-success">Compra de proyectos</button>
--- resources/views/lista2.blade.php
+++ resources/views/lista2.blade.php
@@ -2,40 +2,52 @@
 
 @section('content')
 
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
 
     <div class="section-top-border">
-        <h3 class="mb-30">Lista De Proyectos</h3>
+        <center><h3 class="mb-30">Mis Proyectos Creados</h3></center>
+        <br><br>
         <div class="progress-table-wrap">
             <div class="progress-table">
                 <div class="table-head">
-                    <div class="visit">Nombre</div>
                 </div>
 
 
+                <table align="center" style="width:70%">
+                    <tr><th>Nombre del proyecto</th><th></th><th></th></tr>
                 @foreach ($tipos as $t)
                     @if($t->owner==$id)
                     <div class="table-row">
-                        <div class="visit">{{ $t->titulo }}</div>
+                        <tr>
+                            <td><div class="visit">{{ $t->titulo }}</div></td>
 
                         <div class="serial">
 
                             <form action="{{ route('tabla') }}" method="POST">
                                 @csrf
                                 <input type="hidden" name="id_tipo_proyecto" value="{{ $t->id }}">
-                                <input type="submit" value="Calcular" class="genric-btn danger">
+                                <td><input type="submit" value="Calcular Presupuesto" class="genric-btn danger"></td>
                             </form>
                             @if($t->user==$id)
                             <form action="{{ route('comprar') }}" method="POST">
                                 @csrf
                                 <input type="hidden" name="id_tipo" value="{{ $t->id }}">
-                                <input type="submit" value="Poner en venta" class="genric-btn danger">
+                                <td><input type="submit" value="Poner en venta" class="genric-btn danger"></td>
                             </form>
                                 @endif
                         </div>
+                        </tr>
                     </div>
                     @endif
                 @endforeach
+                </table>
             </div>
+            <br>
 
             <center><a href="{{ url('/home') }}" class="btn btn-info" role="button">Regresar</a></center>
         </div>
